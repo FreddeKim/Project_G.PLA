e4ab7455c4ba1b21b341685f090cb79684afad63
FreddeKim <tktlsqkd1@gmail.com>
2024-05-08 20:16:08 +0900

    0508

--- js/gpla_webevent.js
+++ js/gpla_webevent.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
     searchbar();
+    slider();
 });
 
 
@@ -10,8 +11,49 @@ $(document).ready(function(){
 function searchbar(){
     $('header > div > div > div > button').click(function(){
         $('header > div > div > div > div').toggleClass('searchbar');
-    })
+    });
     $('header > div > div > div > div > form > div > input[type="button"]').click(function(){
         $('header > div > div > div > div').removeClass('searchbar');
-    })
+    });
+};
+// function slidermove(){
+//     $('.sArrowLeft').click(function(){
+//         $('.slidercontainer').css('transform','translate(-100vw)')
+//     });
+//     $('.sArrowRight').click(function(){
+//         $('.slidercontainer').css('transform','translate(100vw)')
+//     });
+// }
+
+
+function slider() {
+    const $sliderWrap = $('.slidercontainer');
+    const $sliderBox = $sliderWrap.find('.containerBox');
+
+    let sliderindex = 0;
+    let sliderCount = $sliderBox.length;
+    let sliderInterval = 6000;
+
+
+    function sliderPrev(){
+        let prevSlide = (sliderindex - 1 + sliderCount) % sliderCount;
+        $sliderBox.eq(sliderindex).fadeOut(800);
+        $sliderBox.eq(prevSlide).fadeIn(800);
+        sliderindex = prevSlide;
+    }
+    function sliderNext(){
+        let nextSlide = (sliderindex + 1) % sliderCount;
+        $sliderBox.eq(sliderindex).fadeOut(800);
+        $sliderBox.eq(nextSlide).fadeIn(800);
+        sliderindex = nextSlide;
+    }
+
+
+    $('.sArrowLeft').click(function(){
+        sliderPrev();
+    });
+    $('.sArrowRight').click(function(){
+        sliderNext();
+    });
+    setInterval(sliderNext , sliderInterval);
 }
